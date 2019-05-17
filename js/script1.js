f6540d6e18fa71a31410c3bde990647ada2511b8
Jamal Hayder <jam392640@gmail.com>
2019-05-18 09:18:11 +0930

    card logo change, css modification, little font changes, navigation bar hover fix

--- js/script1.js
+++ js/script1.js
@@ -1,7 +1,1 @@
-$(document).ready(function() {
-  $('.mobile-button a').click(function(e) {
-	$(this).parent().parent().toggleClass('open');
-  $(this).html($(this).html() == 'Close Menu' ? 'Menu' : 'Close Menu');
-    e.preventDefault();
-  });
-});
+
